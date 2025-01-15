f8df510173687ade947caee1138b8ba2285fcd25
Jonathon Cihlar <36781511+jonathoncihlar@users.noreply.github.com>
2025-01-15 03:59:55 +0000

    Add runner

--- src/main/java/TokenPassRunner.java
+++ src/main/java/TokenPassRunner.java
@@ -8,8 +8,16 @@
  public class TokenPassRunner {
 
     public static void main (String args[]) {
+      TokenPass tp = new TokenPass(5);
+      System.out.println(tp.getPlayerCount());
+      System.out.println(tp.getTokenCount());
+      System.out.println(tp.getBoard());
+      System.out.println(tp.getWinner());
 
+      for (int i = 0; i < 10; i++) {
+         tp.play();
+         System.out.println(tp.getBoard());
+         System.out.println(tp.getWinner());
+      }
     }
-
-
  }
